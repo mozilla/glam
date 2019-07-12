45803fa427ed80ec8eb67418f3a879e77d34b0ed
Rob Hudson <robhudson@mozilla.com>
2019-07-12 11:29:06 -0700

    Switch to airbnb eslint and fix errors

--- functions/db/import_probes.fn.js
+++ functions/db/import_probes.fn.js
@@ -1,36 +1,51 @@
-'use strict';
-
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const https = require("https");
+const https = require('https');
 
-try { admin.initializeApp() } catch (e) { console.log(e) }
+try {
+  admin.initializeApp();
+} catch (e) {
+  console.log(e);
+}
 
+const PROBE_INFO_URL =
+  'https://probeinfo.telemetry.mozilla.org/firefox/all/main/all_probes';
 
-const PROBE_INFO_URL = "https://probeinfo.telemetry.mozilla.org/firefox/all/main/all_probes";
+/**
+ * Store the probe document in the Firestore db.
+ */
+async function storeProbe(key, probe) {
+  const db = admin.firestore();
 
+  return db
+    .collection('firefox-probes')
+    .doc(key.replace('/', '::'))
+    .set(probe);
+}
 
 /**
  * Cloud function to fetch probe info and store it in Firestore.
  */
+// eslint-disable-next-line no-multi-assign
 exports = module.exports = functions.https.onRequest((request, response) => {
-  
-  https.get(PROBE_INFO_URL, (res) => {
+  https
+    .get(PROBE_INFO_URL, res => {
       console.log(res.headers);
       console.log(res.statusCode);
 
-      let data = "";
+      let data = '';
       let probe = null;
 
-      res.on("data", (d) => {
+      res.on('data', d => {
         data += d;
       });
 
-      res.on("end", () => {
+      res.on('end', () => {
+        let j;
         try {
-          var j = JSON.parse(data);
-        } catch (e) {
-          console.error(e);
+          j = JSON.parse(data);
+        } catch (error) {
+          console.error(error);
           response.status(500).send(error.message);
         }
 
@@ -40,8 +55,7 @@ exports = module.exports = functions.https.onRequest((request, response) => {
           probe = {
             name: j[k].name,
             type: j[k].type,
-            description: (
-              j[k].history.nightly ||
+            description: (j[k].history.nightly ||
               j[k].history.beta ||
               j[k].history.release)[0].description
           };
@@ -49,27 +63,18 @@ exports = module.exports = functions.https.onRequest((request, response) => {
           promises.push(storeProbe(k, probe));
         });
 
-        Promise.all(promises).then(values => {
-          return response.status(200).send("Probes imported.");
-        })
-        .catch(error => {
-          console.error(error);
-          response.status(500).send(error.message);
-        });
-
+        Promise.all(promises)
+          .then(() => {
+            return response.status(200).send('Probes imported.');
+          })
+          .catch(error => {
+            console.error(error);
+            response.status(500).send(error.message);
+          });
       });
-  }).on("error", (e) => {
+    })
+    .on('error', e => {
       console.error(e.message);
       response.status(500).send(e.message);
-  });
+    });
 });
-
-/**
- * Store the probe document in the Firestore db.
- */
-async function storeProbe(key, probe) {
-  const db = admin.firestore();
-
-  key = key.replace("/", "::");
-  return db.collection("firefox-probes").doc(key).set(probe);
-}
