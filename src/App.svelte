de5dbde0957e5c8a0621ad4841090f466a80c073
Rob Hudson <robhudson@mozilla.com>
2019-07-26 10:17:22 -0700

    experimentation with svelte and charts

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,37 @@
 <script>
-	export let name;
+    import { db } from './firebase';
+    import Chart from './Chart.svelte';
+
+    let promise = db
+        .collection('release-65')
+        .doc('02415bbe9515d8c1da5cfa072c0b79c9')
+        .get()
+        .then(doc => {
+            console.log(`Queried document: ${doc.id}`);
+            console.log(doc.data());
+            //return (doc.exists ? doc.data() : "Data not found.");
+            return doc.data();
+        })
+        .catch(error => {
+            console.log(error);
+            throw(error);
+        });
+
+    let dummyData = {
+        aggregates: [
+            {key:1, value:2},
+            {key:2, value:4},
+            {key:3, value:8},
+        ]
+    };
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+{#await promise}
+    <p>Querying data...</p>
+{:then data}
+    <div id="charts"></div>
+    <Chart {data} />
+    <Chart data={dummyData} />
+{:catch error}
+    <p>{error.message}</p>
+{/await}
