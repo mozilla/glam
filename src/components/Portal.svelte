235f8ba0d0de72c2e688de575b8500bdc32c2c02
hamilton <hamilton.ulmer@gmail.com>
2019-10-17 16:07:53 -0700

    forgot to add Portal.svelte

---
+++ src/components/Portal.svelte
@@ -0,0 +1,29 @@
+<script>
+import { onMount, onDestroy } from 'svelte';
+
+let ref;
+let portal;
+
+onMount(() => {
+  portal = document.createElement('div');
+  portal.className = 'portal';
+  document.body.appendChild(portal);
+  portal.appendChild(ref);
+});
+
+onDestroy(() => {
+  document.body.removeChild(portal);
+});
+
+</script>
+
+<style>
+    .portal-container { display: none; }
+
+  </style>
+
+<div class="portal-container">
+  <div bind:this={ref}>
+    <slot></slot>
+  </div>
+</div>
