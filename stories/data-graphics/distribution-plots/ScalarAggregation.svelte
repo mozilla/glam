1fd4b383713515a23d191492a3897002299a8b2c
hamilton <hamilton.ulmer@gmail.com>
2019-10-22 06:24:51 -0700

    small cleanup to the ScalarAggregation story

--- stories/data-graphics/distribution-plots/ScalarAggregation.svelte
+++ stories/data-graphics/distribution-plots/ScalarAggregation.svelte
@@ -67,34 +67,42 @@ function togglePercentile(p) {
     width: max-content;
     margin-bottom: var(--space-base);
   }
+
+  .body-content {
+    margin-top: var(--space-4x);
+  }
 </style>
 
 <div class=story>
 
-<div class=time-horizon>
-  <ButtonGroup>
-    <Button toggled={D === 'WEEK'} compact level=medium on:click={() => { setDomain('WEEK'); }}>last week</Button>
-    <Button toggled={D === 'MONTH'} compact level=medium on:click={() => { setDomain('MONTH'); }}>last month</Button>
-    <Button toggled={D === 'ALL_TIME'} compact level=medium on:click={() => { setDomain('ALL_TIME'); }}>all
-    time</Button>
-  </ButtonGroup>
-</div>
+  <h1 class="story__title">probe / <span class=probe-head>browser_engagement_active_ticks</span></h1>
 
-<div class=time-horizon>
-  <ButtonGroup>
-  {#each [5, 25, 50, 75, 95] as p, i (p)}
-    <Button toggled={percentiles.includes(p)} compact level=low on:click={() => { togglePercentile(p); }}>{p}%</Button>
-  {/each}
-  </ButtonGroup>
-</div>
+  <div class=body-content>
+    <div class=time-horizon>
+      <ButtonGroup>
+        <Button toggled={D === 'WEEK'} compact level=medium on:click={() => { setDomain('WEEK'); }}>last week</Button>
+        <Button toggled={D === 'MONTH'} compact level=medium on:click={() => { setDomain('MONTH'); }}>last month</Button>
+        <Button toggled={D === 'ALL_TIME'} compact level=medium on:click={() => { setDomain('ALL_TIME'); }}>all
+        time</Button>
+      </ButtonGroup>
+    </div>
 
-{#each aggs as [aggType, dataset], i (aggType + D)}
-  <h4>{readableAggs[aggType]}</h4>
-  <ScalarAggregationMultiple
-    data={dataset}
-    key={aggType + D}
-    resolution={D}
-    percentiles={percentiles}
-  />
-{/each}
+    <div class=time-horizon>
+      <ButtonGroup>
+      {#each [5, 25, 50, 75, 95] as p, i (p)}
+        <Button toggled={percentiles.includes(p)} compact level=low on:click={() => { togglePercentile(p); }}>{p}%</Button>
+      {/each}
+      </ButtonGroup>
+    </div>
+
+    {#each aggs as [aggType, dataset], i (aggType + D)}
+      <h4>{readableAggs[aggType]}</h4>
+      <ScalarAggregationMultiple
+        data={dataset}
+        key={aggType + D}
+        resolution={D}
+        percentiles={percentiles}
+      />
+    {/each}
+    </div>
 </div>
