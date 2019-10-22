1b1f73bca0e92f4b22d424f8ebe59bd5ab4a7367
hamilton <hamilton.ulmer@gmail.com>
2019-10-21 21:04:40 -0700

    removed extraneous comments in ComparisonMultiple.svelte

--- src/components/data-graphics/ComparisonSummary.svelte
+++ src/components/data-graphics/ComparisonSummary.svelte
@@ -1,5 +1,4 @@
 <script>
-import ComparisonMultiple from './ComparisonMultiple.svelte';
 
 export let left;
 export let right;
@@ -44,46 +43,6 @@ td, th {
 } */
 
 </style>
-<!-- 
-<div class=summary>
-    {#each [[left, 'hovered'], [right, 'latest']] as [focus, label], i}
-      <div class=summary-percentiles>
-        {#if focus}
-          <div class='summary-percentile--full'>
-            {label}
-          </div>
-          <div class='summary-percentile--full' style="border-bottom: 1px solid var(--cool-gray-300)">
-              <span style="text-align: right;font-family: var(--main-mono-font); color: var(--cool-gray-500); font-weight: bold" >
-                  {focus.label.slice(0, 4)}-{focus.label.slice(4,
-                  6)}-{focus.label.slice(6, 8)}{' '}</span> 
-                <span> {focus.label.slice(8, 10)}:</span>
-                <span>{focus.label.slice(10, 12)}:</span>
-                <span>{focus.label.slice(12, 14)}</span>
-          </div>
-          <div class=summary-percentile--bin>
-              # profiles
-            </div>
-            <div class=summary-percentile--value>
-              {focus.audienceSize}
-            </div>
-          {#each percentiles as percentile, i (percentile)}
-            <div class=summary-percentile--bin>
-                {percentile}th %
-              </div>
-              <div class=summary-percentile--value>
-                <div>
-                  {focus.percentiles.find((p) => p.bin === percentile).value}
-                </div>
-                <div>
-                    {focus.percentiles.find((p) => p.bin === percentile).value}
-                  </div>
-              </div>
-          {/each}
-        {/if}
-      </div>
-    {/each}
-  </div> -->
-
 
 <div class=summary>
 
