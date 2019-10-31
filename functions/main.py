5b3e4aa65112a9036d91eef1caddc2aa9db0f9e7
Rob Hudson <robhudson@mozilla.com>
2019-10-31 15:50:29 -0700

    Remove Firebase/Firestore code

--- functions/main.py
+++ functions/main.py
@@ -17,6 +17,82 @@ PROBES_URL = "https://probeinfo.telemetry.mozilla.org/firefox/all/main/all_probe
 PRODUCT_DETAILS_URL = "https://product-details.mozilla.org/1.0/firefox_versions.json"
 latest_nightly = None
 
+# TODO: Remove this.
+# Only import probes in this list.
+PROBES = [
+    "a11y_backplate",
+    "a11y_consumers",
+    "a11y_iatable_usage_flag",
+    "a11y_instantiated_flag",
+    "a11y_isimpledom_usage_flag",
+    "a11y_theme",
+    "a11y_tree_update_timing_ms",
+    "browser_engagement_active_ticks",
+    "browser_engagement_max_concurrent_tab_count",
+    "browser_engagement_max_concurrent_tab_pinned_count",
+    "browser_engagement_max_concurrent_window_count",
+    "browser_engagement_navigation_about_home",
+    "browser_engagement_navigation_about_newtab",
+    "browser_engagement_navigation_contextmenu",
+    "browser_engagement_navigation_searchbar",
+    "browser_engagement_navigation_urlbar",
+    "browser_engagement_navigation_webextension",
+    "browser_engagement_tab_open_event_count",
+    "browser_engagement_tab_pinned_event_count",
+    "browser_engagement_total_uri_count",
+    "browser_engagement_unfiltered_uri_count",
+    "browser_engagement_unique_domains_count",
+    "browser_engagement_window_open_event_count",
+    "cryptominers_blocked_count",
+    "devtools_browserconsole_opened_count",
+    "devtools_onboarding_is_devtools_user",
+    "fxa_configured",
+    "gc_ms",
+    "http_scheme_upgrade_type",
+    "scalar_parent_timestamps_first_paint",
+    "ssl_auth_algorithm_full",
+    "ssl_auth_ecdsa_curve_full",
+    "ssl_auth_rsa_key_size_full",
+    "ssl_bytes_before_cert_callback",
+    "ssl_cert_error_overrides",
+    "ssl_cert_verification_errors",
+    "ssl_cipher_suite_full",
+    "ssl_cipher_suite_resumed",
+    "ssl_ct_policy_compliant_connections_by_ca",
+    "ssl_ct_policy_non_compliant_connections_by_ca",
+    "ssl_handshake_result",
+    "ssl_handshake_type",
+    "ssl_handshake_version",
+    "ssl_initial_failed_cert_validation_time_mozillapkix",
+    "ssl_kea_dhe_key_size_full",
+    "ssl_kea_ecdhe_curve_full",
+    "ssl_kea_rsa_key_size_full",
+    "ssl_key_exchange_algorithm_full",
+    "ssl_key_exchange_algorithm_resumed",
+    "ssl_npn_type",
+    "ssl_ocsp_stapling",
+    "ssl_permanent_cert_error_overrides",
+    "ssl_reasons_for_not_false_starting",
+    "ssl_resumed_session",
+    "ssl_scts_origin",
+    "ssl_scts_per_connection",
+    "ssl_scts_verification_status",
+    "ssl_server_auth_eku",
+    "ssl_succesful_cert_validation_time_mozillapkix",
+    "ssl_symmetric_cipher_full",
+    "ssl_symmetric_cipher_resumed",
+    "ssl_time_until_handshake_finished_keyed_by_ka",
+    "ssl_time_until_ready",
+    "ssl_tls12_intolerance_reason_pre",
+    "ssl_tls13_intolerance_reason_post",
+    "ssl_tls13_intolerance_reason_pre",
+    "ssl_version_fallback_inappropriate",
+    "wr_framebuild_time",
+    "wr_render_time",
+    "wr_scenebuild_time",
+    "wr_sceneswap_time",
+]
+
 
 def retry(exceptions, tries=3, delay=1, backoff=2):
     def fn(f):
@@ -51,26 +127,16 @@ def get_latest_nightly_version():
 
 
 def get_name(name):
-    # Returns a tuple of (<name>, <api_name>)
-    #
-    # The name is with `histogram/` or `scalar/` removed, dots to underscores,
+    # Returns name with `histogram/` or `scalar/` removed, dots to underscores,
     # and lower case.
-    #
-    # The API name is the same as above except s/scalar/scalar_parent_/ to
-    # match the BigQuery table.
-    #
-    # See the bigquery-etl project for the naming transformation.
 
     prefix, name = name.split("/")
 
-    if prefix == "histogram":
+    if prefix in ["histogram", "scalar"]:
         name = name.replace(".", "_").lower()
-        return (name, name)
-    elif prefix == "scalar":
-        name = name.replace(".", "_")
-        return (name, f"scalar_parent_{name}")
+        return name
     else:
-        return (name, name)
+        return name
 
 
 def get_versions(channel, probe):
@@ -102,6 +168,9 @@ def extract():
     probes_dict = json.loads(urllib.request.urlopen(PROBES_URL).read())
     # Filter probes by histograms or scalars only.
     keys = [k for k in probes_dict.keys() if k.startswith(("histogram/", "scalar/"))]
+    # TODO: Remove this.
+    # Temporarily only import probes chosen probes.
+    keys = [k for k in keys if get_name(k) in PROBES]
     # Restructure from one global dict to a list of dicts per probe, with `key`
     # being the original probe dict key.
     probes = [dict(probes_dict[k], key=k) for k in keys]
@@ -119,12 +188,12 @@ def transform(probe):
         or probe["history"].get("release")
     )[0]
     nightly_versions = get_versions("nightly", probe)
-    name, api_name = get_name(probe["key"])
+    name = get_name(probe["key"])
 
     doc = {
         "id": probe["key"].replace("/", "::").lower(),
         "name": name,
-        "apiName": api_name,
+        "apiName": name,
         "description": latest_history["description"],
         "type": probe["type"],
         "kind": latest_history["details"].get("kind"),
