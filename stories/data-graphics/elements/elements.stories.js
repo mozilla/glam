cf40c2dc5d9e31f2f6dcd7a19a7bcddf00ce3549
hamilton <hamilton.ulmer@gmail.com>
2019-12-04 12:31:51 -0800

    moves around data graphic stories and renames them

--- stories/data-graphics/elements/elements.stories.js
+++ stories/data-graphics/elements/elements.stories.js
@@ -1,12 +1,9 @@
 import { storiesOf } from '@storybook/svelte';
 import Heatmap from './Heatmap.svelte';
-import Markers from './Markers.svelte';
 import '../../../public/static/global.css';
 import '../../glean-design-stories.css';
 
-storiesOf('Data Graphics|Small Multiples', module)
+storiesOf('Data Graphics|Elements', module)
   .add('Heatmap', () => ({
     Component: Heatmap,
-  })).add('Markers', () => ({
-    Component: Markers,
   }));
