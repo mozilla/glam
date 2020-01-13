60baf5d5c54c524384dbda075cb5c64a1d26540a
hamilton <hamilton.ulmer@gmail.com>
2020-01-13 09:46:37 -0800

    adds a placeholder for front page while random probes are loading

---
+++ src/app/patterns/defaultview/RandomProbePlaceholder.svelte
@@ -0,0 +1,35 @@
+<style>
+
+@keyframes pulse {
+    0%, 100% {
+        /* opacity: 1; */
+        fill:  var(--cool-gray-150);
+    } 35% {
+        /* opacity:.9; */
+        fill: var(--cool-gray-100);
+    }
+}
+
+svg {
+    fill: var(--cool-gray-150);
+    animation-name: pulse;
+    animation-duration: 1.25s;
+    animation-iteration-count: infinite;
+}
+
+</style>
+
+<svg width=330 height="100%" viewBox="0 0 335 170" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <g transform="matrix(1,0,0,1,-197,-449)">
+        <g transform="matrix(1.00601,0,0,1,-3.1952,0)">
+            <rect rx=5 x="199" y="449" width="333" height="103" />
+        </g>
+        <rect rx=3 x="197" y="564" width="159" height="12"/>
+        <g transform="matrix(1,0,0,1.15385,0,-92.1538)">
+            <rect rx=3 x="197" y="587" width="295" height="12"/>
+        </g>
+        <g  rx=3 transform="matrix(1,0,0,0.8,0,123)">
+            <rect rx=3 x="197" y="605" width="84" height="15"/>
+        </g>
+    </g>
+</svg>
