a9ab2530c06b44b50ffbd06163b152e962ce8efc
hamilton <hamilton.ulmer@gmail.com>
2020-02-08 12:05:29 -0800

    removes transform as prop from CompareOverTimeGraph

--- src/udgl/data-graphics/motion/Springable.svelte
+++ src/udgl/data-graphics/motion/Springable.svelte
@@ -4,8 +4,9 @@ import { spring } from 'svelte/motion';
 
 export let params = { stiffness: 0.8, damping: 0.9 };
 export let value = 0;
+export let from = value;
 
-const springValue = spring(value, params);
+const springValue = spring(from, params);
 $: springValue.set(value);
 
 </script>
