7539e765fdcee817996e400b4faaec9fbf900d5e
Davor Spasovski <davor@spasovski.net>
2020-03-02 15:41:36 -0600

    add process probe filtering

--- src/components/regions/MainSelectors.svelte
+++ src/components/regions/MainSelectors.svelte
@@ -33,14 +33,14 @@ const COMPACT = true;
 
 .pull-right-edge {
   margin-right: calc(var(--space-1h) * -1);
-  display:grid;
+  display: grid;
   align-items: center;
 }
 
 </style>
 
 <div class='main-filters'>
-  <MenuButton  tooltip={'Select a Channel'} compact={COMPACT} offset={OFFSET} location='bottom' alignment='right'>
+  <MenuButton tooltip={'Select a Channel'} compact={COMPACT} offset={OFFSET} location='bottom' alignment='right'>
     <div class=main-filter__label slot="label">{getFieldValueLabel('channel', $store.channel)} <div class=pull-right-edge><DownCarat size=14 /></div></div>
     <div slot="menu">
       <MenuList on:selection={(event) => { store.setField('channel', event.detail.key); }}>
@@ -50,27 +50,38 @@ const COMPACT = true;
             {/each}
         </MenuList>
     </div>
-    </MenuButton>
-    <MenuButton  tooltip={'Select an OS'} compact={COMPACT} offset={OFFSET} location='bottom' alignment='right'>
-        <div class=main-filter__label slot="label">{getFieldValueLabel('os', $store.os)}<div class=pull-right-edge><DownCarat size=14 /></div></div>
-        <div slot="menu">
-            <MenuList on:selection={(event) => { store.setField('os', event.detail.key); }}>
-              {#each CONFIG.fields.os.values as {key, label}, i (key)}
-                <MenuListItem  key={key} value={key}><span class='story-label
-                  first'></span>{label}</MenuListItem>
-                {/each}
-            </MenuList>
-        </div>
-      </MenuButton>
-      <MenuButton  tooltip={'Select an Aggregation Level'} compact={COMPACT}  offset={OFFSET} location='bottom' alignment='left'>
-          <div class=main-filter__label slot="label">{getFieldValueLabel('aggregationLevel', $store.aggregationLevel)}<div class=pull-right-edge><DownCarat size=14 /></div></div>
-          <div slot="menu">
-              <MenuList on:selection={(event) => { store.setField('aggregationLevel', event.detail.key); }}>
-                  {#each CONFIG.fields.aggregationLevel.values as {key, label}, i (key)}
-                  <MenuListItem  key={key} value={key}><span class='story-label
-                    first'></span>{label}</MenuListItem>
-                  {/each}
-              </MenuList>
-          </div>
-        </MenuButton>
+  </MenuButton>
+  <MenuButton tooltip={'Select an OS'} compact={COMPACT} offset={OFFSET} location='bottom' alignment='right'>
+    <div class=main-filter__label slot="label">{getFieldValueLabel('os', $store.os)}<div class=pull-right-edge><DownCarat size=14 /></div></div>
+    <div slot="menu">
+      <MenuList on:selection={(event) => { store.setField('os', event.detail.key); }}>
+        {#each CONFIG.fields.os.values as {key, label}, i (key)}
+          <MenuListItem  key={key} value={key}><span class='story-label
+            first'></span>{label}</MenuListItem>
+          {/each}
+      </MenuList>
+    </div>
+  </MenuButton>
+  <MenuButton tooltip={'Select an Aggregation Level'} compact={COMPACT}  offset={OFFSET} location='bottom' alignment='right'>
+    <div class=main-filter__label slot="label">{getFieldValueLabel('aggregationLevel', $store.aggregationLevel)}<div class=pull-right-edge><DownCarat size=14 /></div></div>
+    <div slot="menu">
+        <MenuList on:selection={(event) => { store.setField('aggregationLevel', event.detail.key); }}>
+          {#each CONFIG.fields.aggregationLevel.values as {key, label}, i (key)}
+          <MenuListItem  key={key} value={key}><span class='story-label
+            first'></span>{label}</MenuListItem>
+          {/each}
+        </MenuList>
+    </div>
+  </MenuButton>
+  <MenuButton tooltip={'Select a Process'} compact={COMPACT}  offset={OFFSET} location='bottom' alignment='left'>
+    <div class=main-filter__label slot="label">{getFieldValueLabel('process', $store.process)}<div class=pull-right-edge><DownCarat size=14 /></div></div>
+    <div slot="menu">
+        <MenuList on:selection={(event) => { store.setField('process', event.detail.key); }}>
+          {#each CONFIG.fields.process.values as {key, label}, i (key)}
+          <MenuListItem  key={key} value={key}><span class='story-label
+            first'></span>{label}</MenuListItem>
+          {/each}
+        </MenuList>
+    </div>
+  </MenuButton>
 </div>
