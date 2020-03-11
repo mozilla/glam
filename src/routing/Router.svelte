42a7a5a092df652dc7309e65daa69c55f81578f6
John Karahalis <john.karahalis@gmail.com>
2020-03-10 23:16:06 -0400

    Fix bug where back button causes refresh

--- src/routing/Router.svelte
+++ src/routing/Router.svelte
@@ -32,9 +32,9 @@
   let visible = false;
 
   function updateQueryString(query) {
-    if (window.history.pushState) {
+    if (window.history.replaceState) {
       const newURL = `${window.location.origin}${window.location.pathname}?${query}`;
-      window.history.pushState(null, null, newURL);
+      window.history.replaceState(null, null, newURL);
     }
   }
 
@@ -71,17 +71,7 @@
   page('/', useComponent(Home));
   page('/:product/:section/:probeName/explore', useComponent(ProbeExplore, 'explore'));
   page('/:product/:section/:probeName/table', useComponent(ProbeTable, 'table'));
-
-  // The page.js documentation implies that this preceding slash is not
-  // necessary,[1] but another guide uses it without explanation.[2] It seems
-  // that the back button does not work if it's ommitted. If it's included, the
-  // back button works but also sometimes triggers a refresh. This could be
-  // related to issue #286.[3]
-  //
-  // [1] https://github.com/visionmedia/page.js/tree/94138d1f3105f9fdb3424f9c0870c1fb90adcd1a#readme
-  // [2] https://jackwhiting.co.uk/posts/setting-up-routing-in-svelte-with-pagejs/
-  // [3] https://github.com/visionmedia/page.js/issues/286
-  page('/*', useComponent(NotFound));
+  page('*', useComponent(NotFound));
 
   page.start();
 </script>
