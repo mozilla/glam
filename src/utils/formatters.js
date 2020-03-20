1d01398c90791c25e6d41925e4eceaa085bc544b
hamilton <hamilton.ulmer@gmail.com>
2020-03-20 14:55:31 -0700

    goes with openjck's tweak

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -11,21 +11,13 @@ export const formatParenPercent = (fmt, v, pad = 0) => {
   let p = '';
   const f = `(${format(fmt)(v)})`;
   if (f.length < pad) {
-    p = Array.from({ length: pad - f.length }).fill(' ').join('');
+    p = ' '.repeat(pad - f.length);
   }
   return `${p}${f}`;
 };
 
-// export const formatBuildIDToDateString = (b) => `${b.slice(0, 4)}-${b.slice(4, 6)}-${b.slice(6, 8)} ${b.slice(8, 10)}h`;
 export const formatBuildIDToDateString = (b) => timeFormat('%Y-%m-%d %H')(b);
 export const ymd = timeFormat('%Y-%m-%d');
 export const timecode = timeFormat('%H:%M:%S');
 export const formatBuildIDToOnlyDate = (b) => ymd(b);
 export const formatToBuildID = timeFormat('%Y%m%d%H%M%S');
-
-// export const formatBuildIDToOnlyDate = (b) => {
-//   const y = b.getFullYear();
-//   const m = b.getMonth() + 1;
-//   const d = b.getDate();
-//   return `${y}-${m}-${d}`;
-// };
