5a8abce5bd94f41a3754376df817cedee548f937
hamilton <hamilton.ulmer@gmail.com>
2020-04-15 13:43:34 -0700

    removes old redundant / missing stories

--- stories/data-tables/data-tables.stories.js
+++ stories/data-tables/data-tables.stories.js
@@ -1,6 +1,5 @@
 import { storiesOf } from '@storybook/svelte';
 import DataTable01 from './DataTable01.svelte';
-import DataTable02 from './DataTable02.svelte';
 import DataTable03 from './DataTable03.svelte';
 import '../../public/static/global.css';
 import '../glean-design-stories.css';
@@ -8,9 +7,6 @@ import '../glean-design-stories.css';
 storiesOf('Data Tables', module)
   .add('Simple Data Table', () => ({
     Component: DataTable01,
-  }))
-  .add('REMOVE WHEN DONE WITH THIS BRANCH', () => ({
-    Component: DataTable02,
   }))
   .add('GLAM probe table', () => ({
     Component: DataTable03,
