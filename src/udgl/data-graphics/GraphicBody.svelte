c528e176a254875523609d8ba0de508a94291b27
hamilton <hamilton.ulmer@gmail.com>
2020-06-03 08:22:04 -0700

    removes udgl/data-graphic

--- src/udgl/data-graphics/GraphicBody.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-import { getContext } from 'svelte';
-
-const key = getContext('key');
-</script>
-
-<g id='graphic-body-content-{key}' style="clip-path: url(#graphic-body-{key})">
-  <slot></slot>
-</g>
