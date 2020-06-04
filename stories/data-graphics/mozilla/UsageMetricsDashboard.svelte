735e7ad7514e03778f55494143d9fadae7f35817
hamilton <hamilton.ulmer@gmail.com>
2020-06-03 19:32:17 -0700

    removes unused stories

--- stories/data-graphics/mozilla/UsageMetricsDashboard.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-import UsageMetricsContent from './UsageMetricsContent.svelte';
-</script>
-
-<div class=story>
-  <h1 class=story__title>Usage Metrics</h1>
-  <UsageMetricsContent />
-</div>
