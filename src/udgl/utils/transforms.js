a02f2a6565b6c218f47465dd209b4e498339adba
Rob Hudson <robhudson@mozilla.com>
2020-09-21 13:38:12 -0700

    Remove unused udgl components

--- src/udgl/utils/transforms.js
+++
@@ -1,9 +0,0 @@
-import produce from 'immer';
-
-export function groupBy(xs, key, transform = (_) => _) {
-  return xs.reduce((rv, x) => {
-    const K = transform(x[key]);
-    (rv[K] = rv[K] || []).push(produce(x, (xi) => xi));
-    return rv;
-  }, {});
-}
