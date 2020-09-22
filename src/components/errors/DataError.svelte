b3ae7e120871523a1cc1df485eed3b04277e2613
Rob Hudson <robhudson@mozilla.com>
2020-09-22 15:45:00 -0700

    Remove temporary data error page

--- src/components/errors/DataError.svelte
+++ src/components/errors/DataError.svelte
@@ -1,14 +1,8 @@
 <script>
-import TempDataForProcessMissingError from './TempDataForProcessMissingError.svelte';
 import DataErrorGeneric from './DataErrorGeneric.svelte';
 
 export let reason;
 export let moreInformation;
-export let statusCode;
-export let product;
 </script>
-{#if statusCode === 404 && product === 'firefox'}
-  <TempDataForProcessMissingError />
-{:else}
-  <DataErrorGeneric {reason} {moreInformation} />
-{/if}
+
+<DataErrorGeneric {reason} {moreInformation} />
