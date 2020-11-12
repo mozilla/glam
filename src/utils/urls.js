2a97fb6d305693a03c999cb1203b730bc22e7a69
Rob Hudson <robhudson@mozilla.com>
2020-11-12 12:06:01 -0800

    Fix #1027: Remove default values from URLs

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,6 @@
+import paramDefaults from '../config/shared';
+
+// Return a shortened URL for bugzilla and github URLs.
 export function extractBugId(url) {
   if (url.includes('bugzilla')) {
     const match = /id=(?<id>\d+)/.exec(url);
@@ -13,4 +16,33 @@ export function extractBugId(url) {
     }
   }
   return url;
+}
+
+// Given an object of query params, return the object stripped of query
+// parameters that are set to their default values.
+export function stripDefaultValues(params) {
+  return Object.keys(params)
+    .filter((k) => {
+      if (Object.keys(paramDefaults).includes(k)) {
+        // If this param has a default value defined.
+        if (paramDefaults[k].isMulti) {
+          // If it has multiple values, use JSON.stringify for an easy
+          // equality check since these are arrays.
+          return (
+            JSON.stringify(params[k]) !==
+            JSON.stringify(paramDefaults[k].defaultValue)
+          );
+        }
+        // It's not `isMulti`, so just compare value to value.
+        return params[k] !== paramDefaults[k].defaultValue;
+      }
+      // If no default defined, return true to always include it in the URL.
+      return true;
+    })
+    .reduce((obj, k) => {
+      return {
+        ...obj,
+        [k]: params[k],
+      };
+    }, {});
 }
