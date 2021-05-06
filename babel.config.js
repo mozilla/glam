3e29f1c9160606e4c4a22ba3f4c71d09d1812f9b
Rob Hudson <robhudson@mozilla.com>
2021-05-06 13:02:51 -0700

    Bump front-end dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
   env: {
     test: {
       plugins: ['@babel/plugin-transform-runtime'],
