c51026b24d09248d565d25d83782379a0e3991b4
Rob Hudson <robhudson@mozilla.com>
2021-05-19 11:43:55 -0700

    Add migration to import initial SHAs

--- glam/tests/conftest.py
+++ glam/tests/conftest.py
@@ -1,6 +1,15 @@
+import os
 import pytest
 
 
 @pytest.fixture(autouse=True)
 def enable_db_access_for_all_tests(db):
     pass
+
+
+def pytest_configure(config):
+    os.environ["PYTEST_RUNNING"] = "true"
+
+
+def pytest_unconfigure(config):
+    os.environ.pop("PYTEST_RUNNING")
