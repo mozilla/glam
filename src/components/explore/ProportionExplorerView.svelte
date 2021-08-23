bd8eda6b60632ba76906f26596ceb0a9115abee2
Linh Nguyen <linhnguyen@mozilla.com>
2021-08-23 11:23:36 -0700

    PAdd context to the filter condition

--- src/components/explore/ProportionExplorerView.svelte
+++ src/components/explore/ProportionExplorerView.svelte
@@ -66,7 +66,7 @@
   }
 
   function filterResponseData(d, agg, key) {
-    // filter out true/false aggregate results in boolean metrics. See: https://github.com/mozilla/glam/pull/1525#issue-714577595
+    // filter out true/false aggregate results in boolean metrics. See: https://github.com/mozilla/glam/pull/1525#discussion_r694135079
     if (d[0].metric_type === 'boolean')
       return d.filter((di) => di.client_agg_type === '');
     // filter by key selected for other probes
