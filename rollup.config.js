8fed5b6cd4b3aa305db3f9681f1b204bc24396cc
Will Lachance <wlachance@mozilla.com>
2021-10-12 17:31:14 -0400

    Update Glean Dictionary URL (#1640)

--- rollup.config.js
+++ rollup.config.js
@@ -34,8 +34,7 @@ export default {
       __BASE_DOMAIN__: production ? '' : 'http://localhost:8000',
       __GA_TRACKING_ID__: process.env.GA_TRACKING_ID,
       __BASE_SEARCH_DOMAIN__: SEARCH_DOMAIN,
-      __GLEAN_DICTIONARY_DOMAIN__:
-        'https://deploy-preview-882--glean-dictionary-dev.netlify.app',
+      __GLEAN_DICTIONARY_DOMAIN__: 'https://dictionary.telemetry.mozilla.org',
     }),
     string({ include: 'src/**/*.tpl' }),
     svelte({
