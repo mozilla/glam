213509a32321ac59e9c9b2988d72ba402d2f2370
Eduardo Filho <edugomfilho@gmail.com>
2022-04-13 13:30:08 -0400

    #955 simplifying markers display logic for fx/fenix

--- src/components/ReleaseVersionMarkers.svelte
+++ src/components/ReleaseVersionMarkers.svelte
@@ -26,24 +26,7 @@
   }
 </script>
 
-{#if $store.product === 'firefox'}
-  <g class="firefox-release-version-markers">
-    {#if markers && markers.length}
-      {#each markers.filter((d) => d.date !== undefined && d.date >= $xScale.domain()[0] && d.date <= $xScale.domain()[1]) as { label, date }, i (date)}
-        <Marker location={date}>
-          {#if labels}
-            {#if $store.productDimensions.channel === 'nightly'}
-              {label + 2}
-            {:else if $store.productDimensions.channel === 'beta'}
-              {label + 1}`
-            {:else}{label}{/if}
-          {/if}
-        </Marker>
-      {/each}
-    {/if}
-  </g>
-{/if}
-{#if $store.product === 'fenix'}
+{#if ['firefox', 'fenix'].includes($store.product)}
   <g class="firefox-release-version-markers">
     {#if markers && markers.length}
       {#each markers.filter((d) => d.date !== undefined && d.date >= $xScale.domain()[0] && d.date <= $xScale.domain()[1]) as { label, date }, i (date)}
