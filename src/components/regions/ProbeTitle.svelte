c793898cc9e8ae2ff2a5eae9891335c18c374859
Linh Nguyen <linhnguyen@mozilla.com>
2022-06-07 15:52:31 -0400

    Add GLEAN visualizer to FOG metric names

--- src/components/regions/ProbeTitle.svelte
+++ src/components/regions/ProbeTitle.svelte
@@ -2,8 +2,19 @@
   import { store } from '../../state/store';
 </script>
 
+<style>
+  .glean {
+    color: var(--pantone-red-600);
+    font-weight: 300;
+    font-size: 0.8em;
+  }
+</style>
+
 <h2 class="probe-details-title">
   {$store.route.view}
   /
   <span>{$store.probe.name}</span>
+  {#if $store.product === 'fog'}
+    <span class="glean">(GLEAN)</span>
+  {/if}
 </h2>
