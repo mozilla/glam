e80add3f1783c76bc7bb95027b6b795324db1064
Eduardo Filho <edugomfilho@gmail.com>
2023-11-21 20:23:42 -0500

    Migrate FOG user counts, sample counts starting v119

--- glam/api/migrations/0027_fix_fog_user_count_after_sampling.py
+++ glam/api/migrations/0027_fix_fog_user_count_after_sampling.py
@@ -2,15 +2,10 @@ from django.db import migrations
 
 configs = [
     {
-        "model": "desktopreleaseaggregation",
-        "pretty_name": "Desktop",
-        "filter": {"os": "Windows"},
-    },
-    {
         "model": "fogaggregation",
         "pretty_name": "FOG",
         "filter": {"os": "Windows", "app_id": "release", "version__gt": 118},
-    },
+    }
 ]
 
 
@@ -23,9 +18,9 @@ def do_migration(apps, fwd=True):
         table_iter = table.iterator(100000)
         for i, instance in enumerate(table_iter):
             if fwd:
-                instance.total_sample *= 10
+                instance.total_users *= 10
             else:
-                instance.total_sample /= 10
+                instance.total_users /= 10
             instance.save()
             if i % 10000 == 0 or i + 1 == total:
                 print(
@@ -35,32 +30,28 @@ def do_migration(apps, fwd=True):
     print("\nRefreshing views...")
 
 
-def multiply_sample_count_by_10(apps, schema_editor):
+def multiply_user_count_by_10(apps, schema_editor):
     do_migration(apps)
 
 
-def divide_sample_count_by_10(apps, schema_editor):
+def divide_user_count_by_10(apps, schema_editor):
     do_migration(apps, False)
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("api", "0025_non_normalized_aggregations"),
+        ("api", "0026_fix_windows_release_sample_count"),
     ]
 
     operations = [
         migrations.RunPython(
-            code=multiply_sample_count_by_10, reverse_code=migrations.RunPython.noop
-        ),
-        migrations.RunSQL(
-            "REFRESH MATERIALIZED VIEW CONCURRENTLY view_glam_desktop_release_aggregation",
-            reverse_sql="REFRESH MATERIALIZED VIEW CONCURRENTLY view_glam_desktop_release_aggregation",
+            code=multiply_user_count_by_10, reverse_code=migrations.RunPython.noop
         ),
         migrations.RunSQL(
             "REFRESH MATERIALIZED VIEW CONCURRENTLY view_glam_fog_aggregation",
             reverse_sql="REFRESH MATERIALIZED VIEW CONCURRENTLY view_glam_fog_aggregation",
         ),
         migrations.RunPython(
-            code=migrations.RunPython.noop, reverse_code=divide_sample_count_by_10
+            code=migrations.RunPython.noop, reverse_code=divide_user_count_by_10
         ),
     ]
