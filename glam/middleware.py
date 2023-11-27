45682dbebb01dab84b3e2dd0534a7a7d9e567e50
Linh Nguyen <linhnguyen@mozilla.com>
2023-11-27 18:12:18 -0500

    Change default port of local nginx server

--- glam/middleware.py
+++ glam/middleware.py
@@ -1,7 +1,7 @@
 def dev_cors_middleware(get_response):
     """
     Adds CORS headers for local testing only to allow the frontend, which is
-    served on port 5000, to access the API, which is served on port 8000.
+    served on port 3000, to access the API, which is served on port 8000.
     """
 
     def middleware(request):
