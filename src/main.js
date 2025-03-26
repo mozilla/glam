3c9c9e0c6cd7047e59afa69c8ead4d8b6428bfe2
Eduardo Filho <edugomfilho@gmail.com>
2025-03-26 11:33:20 -0400

    Style

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 import Glean from '@mozilla/glean/web';
 import Router from './routing/Router.svelte';
 
-if (navigator.doNotTrack !== '1' && window.doNotTrack !== '1' && navigator.msDoNotTrack !== '1') {
+if (
+  navigator.doNotTrack !== '1' &&
+  window.doNotTrack !== '1' &&
+  navigator.msDoNotTrack !== '1'
+) {
   console.log('Initializing GleanJS.');
   Glean.initialize('glam', true, {
     enableAutoElementClickEvents: true,
     enableAutoPageLoadEvents: true,
   });
 } else {
-  console.warn('Skipping GleanJS initialization because Do Not Track is enabled in the browser.');
+  console.warn(
+    'Skipping GleanJS initialization because Do Not Track is enabled in the browser.'
+  );
 }
 const router = new Router({
   target: document.body,
