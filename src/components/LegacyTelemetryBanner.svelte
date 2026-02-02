bb526e7ad7955c30168176b90b66ae8c1a7f527c
Chris H-C <chutten@users.noreply.github.com>
2026-02-02 15:16:49 -0500

    Update Legacy Telemetry Banner to past tense

--- src/components/LegacyTelemetryBanner.svelte
+++ src/components/LegacyTelemetryBanner.svelte
@@ -53,8 +53,8 @@
   <div class="legacy-banner__label">Legacy Telemetry</div>
   {#if mirrorMetric}
     <p class="legacy-banner__copy">
-      Desktop Firefox Legacy Telemetry is deprecated, new GLAM data will stop
-      being aggregated for this probe in January 2026. Consider using
+      Desktop Firefox Legacy Telemetry is deprecated, new GLAM data stopped
+      being aggregated for this probe on January 26, 2026. Consider using
       <a
         href={`https://glam.telemetry.mozilla.org/fog/probe/${mirrorMetric}/explore`}
         target="_blank">the Glean version of this probe</a
@@ -63,8 +63,8 @@
     </p>
   {:else}
     <p class="legacy-banner__copy">
-      Desktop Firefox Legacy Telemetry is deprecated, new GLAM data will stop
-      being aggregated for this probe in January 2026.
+      Desktop Firefox Legacy Telemetry is deprecated, new GLAM data stopped
+      being aggregated for this probe on January 26, 2026.
     </p>
   {/if}
 </div>
