3289fe0b674eb00acc80df1e4c25cdcbfd8caaf1
Eduardo Filho <edugomfilho@gmail.com>
2026-04-30 14:38:41 -0400

    aggKey on url

--- src/config/glean-base.js
+++ src/config/glean-base.js
@@ -169,6 +169,7 @@ export default {
       hov: storeValue.hov,
       currentPage: storeValue.currentPage,
       normalizationType: storeValue.productDimensions.normalizationType,
+      aggKey: storeValue.aggKey,
     };
     return stripDefaultValues(params, {
       ...sharedDefaults,
